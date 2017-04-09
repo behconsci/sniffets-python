646f09a1bec768aeaa18e4d58a57dd550a7eaeac
behconsci <behconsci@gmail.com>
2017-04-09 16:36:46 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ except IOError:
 setup(
     name='sniffets',
     packages=['sniffets'],
-    version='0.1',
+    version='0.1.1',
     long_description=long_description,
     description='Python lib for sniffets.com',
     author='Doniyor Jurabayev',
     author_email='behconsci@gmail.com',
     url='https://github.com/behconsci/sniffets-python',
-    download_url='https://github.com/behconsci/sniffets-python/archive/0.1.tar.gz',
+    download_url='https://github.com/behconsci/sniffets-python/archive/0.1.1.tar.gz',
     keywords=['track', 'monitor', 'bug'],
     classifiers=[],
     install_requires=[
